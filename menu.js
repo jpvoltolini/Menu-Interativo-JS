a02cf8a8dcce0fba23a72bfef0732c334a5ce7af
João Pedro Voltolini <112399200+jpvoltolini@users.noreply.github.com>
2023-05-10 14:57:44 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,30 @@
+let opcao = '5'
+
+do {
+    opcao = prompt(
+        "Seja bem-vindo \n"+
+        "\nEscolha umas das opções abaixo"+
+        "\n1. INICIO"+
+        "\n2. ENTRAR/ CADASTRAR"+
+        "\n3. INFO"+
+        "\n4. CONFIGURAÇÕES"+
+        "\n5. ENCERRAR" )    
+    switch(opcao){
+        case "1":
+        alert("Opção Inacessível no momento")
+        break;
+        case "2":
+        alert("Opção Inacessível no momento")
+        break;
+        case "3":
+        alert("Opção Inacessível no momento")
+        break;
+        case "4":
+        alert("Opção Inacessível no momento")
+        break;
+        default:
+        alert("Menu encerrado. muito obrigado!")
+        alert("encerrando...")
+    }
+
+} while (opcao!=='5');
